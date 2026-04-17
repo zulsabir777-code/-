5aa0ec9736ad84985908287e74a3023ad9873298
zulsabir777-code <zulsabir777@gmail.com>
2026-04-17 19:29:14 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '120363424450195993@newsletter',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
