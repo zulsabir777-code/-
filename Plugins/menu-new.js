6fcac23d87691f6c5e71c69d3d60ee0d385b06ad
zulsabir777-code <zulsabir777@gmail.com>
2026-04-18 10:20:51 +0500

    Update menu-new.js

--- Plugins/menu-new.js
+++ Plugins/menu-new.js
@@ -39,8 +39,8 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterJid: '120363424450195993@newsletter',
+                newsletterName: config.Sabir-Botz,
                 serverMessageId: 143
             }
         };
